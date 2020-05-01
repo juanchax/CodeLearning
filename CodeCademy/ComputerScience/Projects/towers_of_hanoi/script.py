3e55b289a7f6fc72761e69f1e5eec38e281981dc
juanchax <juanchax@gmail.com>
2020-05-01 00:40:41 -0300

    -

---
+++ CodeCademy/ComputerScience/Projects/towers_of_hanoi/script.py
@@ -0,0 +1,20 @@
+from stack import Stack
+
+print("\nLet's play Towers of Hanoi!!")
+
+#Create the Stacks
+stacks = []
+left_stack = Stack('Left')
+middle_stack = Stack('Middle')
+right_stack = Stack('Right')
+stacks += [left_stack, middle_stack, right_stack]
+
+#Set up the Game
+
+num_disks = int(input('\nHow many disks would you like to play with?\n'))
+
+
+#Get User Input
+
+
+#Play the Game
