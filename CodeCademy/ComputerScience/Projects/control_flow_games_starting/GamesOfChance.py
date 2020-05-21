32bbafc97f263ce6a8e9bc903d8271ba30a90c78
juanchax <juanchax@gmail.com>
2020-05-21 02:18:49 -0300

    Update GamesOfChance.py

--- CodeCademy/ComputerScience/Projects/control_flow_games_starting/GamesOfChance.py
+++ CodeCademy/ComputerScience/Projects/control_flow_games_starting/GamesOfChance.py
@@ -103,7 +103,7 @@ print(' -- Pick Cards -- ')
 print(message)
 print(' ---------------- ')
 
-def roulette(bet_amount, bet, money):
+def roulette(bet_amount, bet, money): # <<--- this function is incomplete! DO NOT USE!!!
     bet_amount = bet_amount
     red_numbers = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
     black_numbers = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
