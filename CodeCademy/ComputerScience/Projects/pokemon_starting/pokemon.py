644ee2d64f95bc8b4e7121df24fd93951861ac9a
juanchax <juanchax@gmail.com>
2020-05-22 02:49:51 -0300

    -

--- CodeCademy/ComputerScience/Projects/pokemon_starting/pokemon.py
+++ CodeCademy/ComputerScience/Projects/pokemon_starting/pokemon.py
@@ -1,8 +1,8 @@
-# Become A Pokémon Master
+# Become A Pokémon Master ---
 #
 # Overview
 # This project is slightly different than others you have encountered thus far on
-# Codecademy. Instead of a step-by-step tutorial, this project contains a series
+# Codecademy. Insteads of a step-by-step tutorial, this project contains a series
 # of open-ended requirements which describe the project you’ll be building. There
 # are many possible ways to correctly fulfill all of these requirements, and you
 # should expect to use the internet, Codecademy, and other resources when you
